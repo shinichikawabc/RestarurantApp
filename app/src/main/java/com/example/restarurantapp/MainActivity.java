7c2138585bccb5a6ee97c8aa40c54330ebb46a78
shinyi01 <shinyi01@students.cs.ubc.ca>
2021-10-19 21:50:35 -0700

    made buggy add button

--- app/src/main/java/com/example/restarurantapp/MainActivity.java
+++ app/src/main/java/com/example/restarurantapp/MainActivity.java
@@ -6,8 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public MainActivity getThis() {
+        return this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,21 @@ public class MainActivity extends AppCompatActivity {
         table1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 startActivity(new Intent(MainActivity.this,Pop.class));
+            }
+        });
+
+        ImageButton addButton = (ImageButton) findViewById(R.id.imageButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button myButton = new Button(getThis());
+                myButton.setText("Press Me");
+                LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
+                layout.addView(myButton);
+
             }
         });
 
